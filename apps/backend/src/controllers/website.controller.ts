ca4cacea132efdb1139a90892c8279ded8b1d2ba
ankur <ankurdeep03386@gmail.com>
2026-07-16 19:50:53 +0530

    merged changes

--- apps/backend/src/controllers/website.controller.ts
+++ apps/backend/src/controllers/website.controller.ts
@@ -1,27 +1,34 @@
 import {client} from "@repo/db/client"  
 import { NextFunction,Response,Request } from "express"
-import { addWebsiteSchema } from "../types/type"
-import { includes } from "zod"
+import { addWebsiteSchema } from "../types/type";
 
 
 const addWebsite =async(req:Request,res:Response,next:NextFunction)=>{
 
-const parsedData= addWebsiteSchema.safeParse(req.body)
-if(!parsedData.success){
-	return res.status(400).json("send correct url")
-}
-if(!req.userId){
-	return  res.status(400).json("send correct token")
-}
-const web= await client.website.create({
-	data:{
-		url: parsedData.data.url,
-		timeAdded: new Date(),
-		user_id: req.userId
+try {
+		const parsedData = addWebsiteSchema.safeParse(req.body)
+		if(!parsedData.success){
+			return res.status(400).json("send correct crendentials")
+		}
+	
+	if(!req.userId){
+		return res.status(403).json("token not sent");
 	}
-})
-
-return res.status(200).json({id: web.id})
+	
+	const web= await client.website.create({
+		data:{
+			url: parsedData.data.url,
+			timeAdded: new Date(),
+			user_id: req.userId!
+		}
+	})
+	
+	return res.status(200).json("entry made")
+} catch (error) {
+	return res.status(500).json({
+        message: "Internal server error"
+    });
+}
 }
 
 interface websiteId{
--- apps/backend/src/types/type.ts
+++ apps/backend/src/types/type.ts
@@ -4,12 +4,13 @@ export const signUpSchema=z.object({
     password: z.string().min(5)
 })
 export const signInSchema= z.object({
-   username: z.string(),
-   password: z.string() 
+    username: z.string().min(4),
+    password: z.string().min(5)
 })
 export const addWebsiteSchema= z.object({
     url: z.string()
 })
+
 declare global{
     namespace Express{
         interface Request{
